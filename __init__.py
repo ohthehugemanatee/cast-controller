21209a286c2750b0c70b93eb069b2a3d3f5ea3c0
Campbell Vertesi <campbell@vertesi.com>
2021-10-22 21:00:10 +0000

    Make http requests asynchronously

--- __init__.py
+++ __init__.py
@@ -5,6 +5,8 @@ import requests as req
 from collections import deque
 from dataclasses import dataclass, asdict
 from time import sleep
+import aiohttp
+import asyncio
 from evdev import InputDevice, list_devices, ecodes, categorize
 
 CODE_MAP_CHAR = {
@@ -40,7 +42,6 @@ class LEDPreset:
     secondary_speed: float = 0.05
     brightness: float = 1.0
     color_temp: int = 6000
-    gamma: float = 1.0
     saturation: float = 1.0
 
 
@@ -48,10 +49,31 @@ class LEDs:
     currentSettings = {}
 
     def apply(self, new_preset: LEDPreset):
+        requests_to_send = {}
         for k, v in asdict(new_preset).items():
             if k not in self.currentSettings or self.currentSettings[k] is not v:
-                send_request(k, v)
-                self.currentSettings[k] = v
+                requests_to_send[k] = v
+        asyncio.run(self.send_requests(requests_to_send))
+
+    async def send_request(self, session, k, v):
+        url = 'http://localhost/setparam?key={}&value={}'.format(k, v)
+        async with session.get(url) as resp:
+            response_text = await resp.text()
+        if resp.status == 200:
+            return k
+        print('Failed to set {}, response status {}: {}'.format(
+            k, resp.status, response_text))
+
+    async def send_requests(self, requests_to_send=dict):
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for k, v in requests_to_send.items():
+                tasks.append(asyncio.ensure_future(
+                    self.send_request(session, k, v)))
+            keys_set = await asyncio.gather(*tasks)
+            if keys_set is not None:
+                for key in keys_set:
+                    self.currentSettings[key] = requests_to_send[key]
 
 
 StageLights = LEDs()
